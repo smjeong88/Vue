ef4339b6b1552662a63db1c2cce0266cc709cf9b
smjeong88 <smvtr123@naver.com>
2021-03-30 01:04:29 +0900

    vue first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Welcome to Home {{city}}!</h1>
+        <input type="text" v-model="input" />
+        <button type="button" @click="getData">Get</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                title : "개발자의 품격",
+                city : "Seoul",
+                input : "정세민", 
+            };
+        },
+        methods:{
+            getData(){
+                alert(this.input);
+            }
+        },
+    };
+</script>
